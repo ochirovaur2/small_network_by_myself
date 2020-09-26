95d1ebe235a808755aa9cfdf527253e8a579bb5a
Ochirov Aiur <ochirovaur2@mail.ru>
2020-09-26 14:36:51 +0700

    changed

--- first.py
+++ first.py
@@ -15,27 +15,37 @@ output_data = np.array( [[0, 1, 1, 0]] ).T
 weights_1_1 = 2 * np.random.random_sample((3, 1)) - 1
 weights_1_2 = 2 * np.random.random_sample((3, 1)) - 1
 
-learing_rate = 0.01
+learing_rate = 0.001
 
 
 
 for i in range(10000):
 	num_i = 0
 	weights_1_1_delta = []
-
+	weights_1_2_delta = []
 	for iter in input_data:
 
-		res = segmoid(np.dot(input_data[num_i], weights_1_1)) 
-		error_of_layer_1_1 = res - output_data[num_i]
 
-		weights_1_1_delta.append(get_derivative_of_the_segmoid(res) * error_of_layer_1_1)
+
+		res_of_layer_1_1 = segmoid(np.dot(input_data[num_i], weights_1_1)) 
+		error_of_layer_1_1 = res_of_layer_1_1 - output_data[num_i]
+		weights_1_1_delta.append(get_derivative_of_the_segmoid(res_of_layer_1_1) * error_of_layer_1_1)
+		
+		res_of_layer_1_2 = segmoid(np.dot(input_data[num_i], weights_1_2)) 
+		error_of_layer_1_2 = res_of_layer_1_2 - output_data[num_i]
+		weights_1_2_delta.append(get_derivative_of_the_segmoid(res_of_layer_1_2) * error_of_layer_1_2)
+		
 		num_i+=1
 
 	weights_1_1_delta = np.array(weights_1_1_delta) 
 	weights_1_1 -= np.dot(input_data.T, weights_1_1_delta)
+
+	weights_1_2_delta = np.array(weights_1_2_delta) 
+	weights_1_2 -= np.dot(input_data.T, weights_1_2_delta)
 	
 
 print("Weights after training")
+print(weights_1_2)
 print(weights_1_1)
 print(segmoid(np.dot([1,1 ,0], weights_1_1)))
 
@@ -50,7 +60,7 @@ print(segmoid(np.dot([1,1 ,0], weights_1_1)))
 # input_of_layer_2_1 = np.array([[layer_1_1, layer_1_2]])
 # # print(input_of_layer_2_1)
 # # layer_2_1 = segmoid(np.dot(input_data, weights_1_1))
-# # weights_1_1_delta = get_derivative_of_the_segmoid(res) * error
+# # weights_1_1_delta = get_derivative_of_the_segmoid(res_of_layer_1_1) * error
 
 # # weights_1_1 -= np.dot(input_data.T, weights_1_1_delta) * learing_rate
 
